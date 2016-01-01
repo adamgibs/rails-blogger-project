65c0a00ca59b13f5cb3757b8bd7aef1faa2c02c2
Adam G <adamgibs@outlook.com>
2016-01-01 12:08:47 -0500

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,13 @@ Rails.application.routes.draw do
   end
  
 end
+
+Blogger::Application.routes.draw do
+
+  root to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
+  resources :tags
+
+end
